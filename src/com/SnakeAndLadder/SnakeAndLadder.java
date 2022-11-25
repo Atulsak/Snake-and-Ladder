32014a1462f29078e484f883e05cca6beae44989
Atulsak <sakhareat@gmail.com>
2022-11-25 15:30:50 +0530

    ADDED: Play Option to game

--- src/com/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/SnakeAndLadder/SnakeAndLadder.java
@@ -1,16 +1,32 @@
 package com.SnakeAndLadder;
 
 public class SnakeAndLadder {
-	public class SnakeAndLadderGame {
-		public static int playerPosition;
-		
-		SnakeAndLadderGame() {
-			this.playerPosition = 0;
-			System.out.println("Snake and Ladder game starts player position is "+this.playerPosition);
-		 }
-		
-		public int dice() {
-			return (int) Math.floor(Math.random() * 10) % 6 + 1;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2;
+	public static int playerPosition;
+	
+	 SnakeAndLadder() {
+		this.playerPosition = 0;
+		System.out.println("Snake and Ladder game starts player position is "+this.playerPosition);
+	}
+	
+	public int dice() {
+		return (int) Math.floor(Math.random() * 10) % 6 + 1;
+	}
+	
+	public void play() {
+		int crrDice = this.dice();
+		int playOption = (int) Math.floor(Math.random() * 10) % 3;
+		switch (playOption) {
+			case LADDER:
+				System.out.println("Play option is Ladder the player moves ahead by the "+crrDice+" number of position");
+				break;
+			case SNAKE:
+				System.out.println("Play option is Snake the player moves behind by the "+crrDice+" number of position");
+				break;
+			default:
+				System.out.println("Play option is No Play the player stays in the same position");
 		}
-    }
+	}
+	
 }
