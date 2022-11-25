d250e4da8f95af291b649db55e7b7c5cca5ce62c
Atulsak <sakhareat@gmail.com>
2022-11-25 15:26:44 +0530

    ADDED:Dice to Snake And Ladder Game

--- src/com/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/SnakeAndLadder/SnakeAndLadder.java
@@ -1,5 +1,16 @@
 package com.SnakeAndLadder;
 
 public class SnakeAndLadder {
-
+	public class SnakeAndLadderGame {
+		public static int playerPosition;
+		
+		SnakeAndLadderGame() {
+			this.playerPosition = 0;
+			System.out.println("Snake and Ladder game starts player position is "+this.playerPosition);
+		 }
+		
+		public int dice() {
+			return (int) Math.floor(Math.random() * 10) % 6 + 1;
+		}
+    }
 }
